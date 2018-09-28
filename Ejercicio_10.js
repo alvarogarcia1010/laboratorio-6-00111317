bd3ae2edbf5a81de242651935f6bc351c8232f2f
alvarogarcia1010 <00111317@uca.edu.sv>
2018-09-28 08:39:10 -0600

    Ejercicio 10 y 11

---
+++ Ejercicio_10.js
@@ -0,0 +1,6 @@
+function userCreate (nombre, apellido, fechaNacimiento, numeroTelefono, correoElectronico )
+{
+    var user = {nombre, apellido, fechaNacimiento, numeroTelefono, correoElectronico};
+
+    return user;
+}
---
+++ Ejercicio_11.js
@@ -0,0 +1,6 @@
+function userInsert (nombre, apellido, fechaNacimiento, numeroTelefono, correoElectronico )
+{
+    var user = {nombre, apellido, fechaNacimiento, numeroTelefono, correoElectronico};
+
+    return user;
+}
